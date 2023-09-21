c5b7532afac516d9b32f294dddf5db37ac4783c4
Fernando Guimarães <fguimara@redhat.com>
2023-09-21 13:48:39 -0300

    versao 3

--- src/main/java/org/acme/rest/client/AMQFruitService.java
+++ src/main/java/org/acme/rest/client/AMQFruitService.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.Produces;
 import java.util.Set;
 
 import org.acme.rest.json.*;
